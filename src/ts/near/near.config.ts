d53a981d0a9e48870ceb6dd4a4c617265120898a
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-30 18:17:10 -0500

    cons

--- src/ts/near/near.config.ts
+++ src/ts/near/near.config.ts
@@ -1,10 +1,4 @@
 // near.config
-const result = near.config({ networkId: "mainnet" });
-console.log('near.config({ networkId: "mainnet" }) =>', result);
-
-// Runtime Safety + Console Log
-if (typeof near === "undefined") {
-    throw new Error("Global 'near' object is not available");
-  } else {
-    console.log("✅ Global 'near' object is available and ready to use.");
-  }
+// near.config({ networkId: "mainnet" });
+console.log('near.config({ networkId: "mainnet" })');
+console.log(near.config({ networkId: "mainnet" }));
