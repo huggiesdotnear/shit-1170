9160d44eaf6884970b5c47d4e7d6ee0f57c5c231
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 12:09:33 -0500

    rm he

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './css/main.css';
 import './ts/hello.ts';
 import './ts/near/near.config.ts';
 // app ui
-import App_header from './tsx/app_header.tsx';
 import App_section_route from './tsx/app_section_routing.tsx';
 import App_section_footer from './tsx/app_footer.tsx';
 
@@ -12,7 +11,6 @@ import App_section_footer from './tsx/app_footer.tsx';
 const App = () => {
   return (
     <article>
-      <App_header/>
       <App_section_route/>
       <App_section_footer/>
     </article>
