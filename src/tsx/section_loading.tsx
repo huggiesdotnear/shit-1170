f15e53b66c05b7878002d5408335dbab84c974f1
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 10:30:55 -0500

    Update section_loading.tsx

--- src/tsx/section_loading.tsx
+++ src/tsx/section_loading.tsx
@@ -3,11 +3,15 @@ import { HUGGIES_SLOGANS } from '../ts/slogans';
 
 // App_section_loading.tsx
 const App_section_loading: FunctionalComponent<{ path?: string }> = () => {
-  return (
-    <section>
-      <h1>{HUGGIES_SLOGANS[5]}</h1>
-    </section>
-  );
+    // Get random slogan each time component renders
+    const randomIndex = Math.floor(Math.random() * HUGGIES_SLOGANS.length);
+    const randomSlogan = HUGGIES_SLOGANS[randomIndex];
+
+    return (
+        <section>
+            <h1>{randomSlogan}</h1>
+        </section>
+    );
 };
 
 export default App_section_loading;
