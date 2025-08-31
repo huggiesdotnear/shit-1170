0a7615df81d6f267f807139b3b8ecb8a8582e51f
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 10:06:14 -0500

    Create section_routing.tsx

---
+++ src/tsx/section_routing.tsx
@@ -0,0 +1,12 @@
+
+
+// App_section_swap
+const App_section_swap = () => {
+    return (
+        <section>
+            <h1>Routing COMING SOON</h1>
+        </section>
+    );
+  };
+  
+  export default App_section_swap;
