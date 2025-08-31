766814ea69a16ce13a0cf2236b3901c40e3b4e5d
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 10:00:38 -0500

    fo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './ts/near/near.config.ts';
 // app ui
 // import App_header from './tsx/app_header.tsx';
 import App_section_loading from './tsx/section_loading.tsx';
+import App_section_swap from './tsx/section_swap.tsx';
+import App_section_footer from './tsx/app_footer.tsx';
 
 
 // App
@@ -13,6 +15,8 @@ const App = () => {
     <article>
       {/* <App_header/> */}
       <App_section_loading/>
+      <App_section_swap/>
+      <App_section_footer/>
     </article>
   );
 };
---
+++ src/tsx/app_footer.tsx
@@ -0,0 +1,11 @@
+
+
+
+// App_footer
+const App_footer = () => {
+    return (
+      <footer>COPYRIGHT 2025 BY SLEET.NEAR</footer>
+    );
+  };
+  
+  export default App_footer;
