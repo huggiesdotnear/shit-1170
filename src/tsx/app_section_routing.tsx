d71b9fa304ccedf64e31893306cb784f16e93b60
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 10:13:55 -0500

    i

--- src/tsx/app_section_routing.tsx
+++ src/tsx/app_section_routing.tsx
@@ -1,3 +1,7 @@
+import App_section_loading from './section_loading.tsx';
+import App_section_swap from './section_swap.tsx';
+import App_section_info from './section_info.tsx';
+
 
 
 // App_section_route
---
+++ src/tsx/section_info.tsx
@@ -0,0 +1,12 @@
+
+
+// App_section_info
+const App_section_info = () => {
+    return (
+        <section>
+            <h1>on chain info COMING SOON</h1>
+        </section>
+    );
+  };
+  
+  export default App_section_info;
