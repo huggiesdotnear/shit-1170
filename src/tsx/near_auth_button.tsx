8bd982155a3fffad75d0168e5575715a9a7d7180
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-30 20:36:02 -0500

    near auth buuton files

---
+++ src/tsx/near_auth_button.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+// App
+const App_NEAR_AUTH_BUTTON = () => {
+    return (
+      <div className="content">
+        <h1>Rsbuild with Preact</h1>
+        <p>Start building amazing things with Rsbuild.</p>
+      </div>
+    );
+  };
+  
+  export default App_NEAR_AUTH_BUTTON;
