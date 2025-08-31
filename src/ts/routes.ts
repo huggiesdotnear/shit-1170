be2b507e032a0701300dcbf7d76527f852f494e0
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 11:56:42 -0500

    Update routes.ts

--- src/ts/routes.ts
+++ src/ts/routes.ts
@@ -2,7 +2,7 @@
 export const ROUTES = {
     home: { path: '/', label: 'HOME' },
     swap: { path: '/swap', label: 'SWAP' },
-    info: { path: '/info', label: 'INFO' },
+    info: { path: '/info', label: 'ON CHAIN INFO' },
   } as const;
   
   export type RouteKey = keyof typeof ROUTES;
