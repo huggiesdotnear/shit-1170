95e1a4263055a07695b575891c904c130362e5f0
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-30 22:58:30 -0500

    css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './css/main.css';
 import './ts/hello.ts';
 import './ts/near/near.config.ts';
 // app ui
-import App_header from './tsx/app_header.tsx';
+// import App_header from './tsx/app_header.tsx';
 import App_section_loading from './tsx/section_loading.tsx';
 
 
@@ -11,7 +11,7 @@ import App_section_loading from './tsx/section_loading.tsx';
 const App = () => {
   return (
     <article>
-      <App_header/>
+      {/* <App_header/> */}
       <App_section_loading/>
     </article>
   );
