b3d3fbc80e121fc9dffb3992c12b743077ad7047
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-30 22:32:54 -0500

    Update section_loading.tsx

---
+++ src/tsx/section_loading.tsx
@@ -0,0 +1,12 @@
+import { HUGGIES_SLOGANS } from '../ts/slogans';
+
+// App_header
+const App_section_loading = () => {
+    return (
+        <section>
+            <h1>{HUGGIES_SLOGANS[5]}</h1>
+        </section>
+    );
+  };
+  
+  export default App_section_loading;
