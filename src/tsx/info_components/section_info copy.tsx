aa73a64b481101747f44df5d4500c20bd1fb427a
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 11:59:02 -0500

    infop

---
+++ src/tsx/info_components/section_info copy.tsx
@@ -0,0 +1,12 @@
+import type { FunctionalComponent } from 'preact';
+
+// App_section_info.tsx
+const App_section_info: FunctionalComponent<{ path?: string }> = () => {
+    return (
+        <section>
+            <h1>on chain info COMING SOON</h1>
+        </section>
+    );
+  };
+  
+  export default App_section_info;
