dbdf662c962fa8e3af9059db102e1312a6352c0a
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 10:47:56 -0500

    r

--- src/tsx/app_nav.tsx
+++ src/tsx/app_nav.tsx
@@ -1,14 +1,15 @@
 
 
-
 // App_nav
 const App_nav = () => {
-    return (
-      <nav>
-        <a href="/">SWAP</a>
-        <a href="/info">INFO</a>
-      </nav>
-    );
-  };
-  
-  export default App_nav;
+
+
+  return (
+    <nav>
+      <a href="/swap">SWAP</a>
+      <a href="/info">INFO</a>
+    </nav>
+  );
+};
+
+export default App_nav;
