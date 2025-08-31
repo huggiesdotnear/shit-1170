4037c29c42016cf259e974379269e14457040616
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-30 21:16:47 -0500

    css

--- src/tsx/app_header.tsx
+++ src/tsx/app_header.tsx
@@ -1,10 +1,12 @@
 import App_NEAR_AUTH_BUTTON from './near_auth_button.tsx';
-
+import shit_white_icon_png from '../img/shit_white_icon.png';
 
 // App_header
 const App_header = () => {
     return (
       <header>
+        <img src={shit_white_icon_png}/>
+        <h1>HUGGIES</h1>
         <App_NEAR_AUTH_BUTTON/>
       </header>
     );
