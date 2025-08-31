c4eceb5be63bfe2d22f5899ace24fc04f5a4ba4a
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 12:56:01 -0500

    hug

--- src/tsx/section_huggies.tsx
+++ src/tsx/section_huggies.tsx
@@ -1,13 +1,17 @@
 import type { FunctionalComponent } from 'preact';
 import shit_white_icon_svg from '../img/shit_icon.svg';
+import { ROUTES } from '../ts/routes';
+
 
 
 // App_section_swap.tsx
 const App_section_huggies: FunctionalComponent<{ path?: string }> = () => {
   return (
-    <section>
-      <img src={shit_white_icon_svg} alt="Huggies Logo"/>
+    <section className="section_huggies">
+      <img src={shit_white_icon_svg} alt="Huggies Logo" />
       <h1>HUGGIES</h1>
+      <a href={ROUTES.swap.path}>{ROUTES.swap.label}</a>
+      <a href={ROUTES.info.path}>{ROUTES.info.label}</a>
     </section>
   );
 };
