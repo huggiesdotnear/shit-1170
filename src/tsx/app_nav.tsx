f11b8e1218365802c6f9469e7e0c58ab31c4ac13
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 10:40:17 -0500

    Create app_nav.tsx

---
+++ src/tsx/app_nav.tsx
@@ -0,0 +1,14 @@
+
+
+
+// App_nav
+const App_nav = () => {
+    return (
+      <nav>
+        <a href="/">SWAP</a>
+        <a href="/info">INFO</a>
+      </nav>
+    );
+  };
+  
+  export default App_nav;
