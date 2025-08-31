8c711c4fceedd6945147b20eb09b65f84726ca53
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 15:39:58 -0500

    rm rh

--- src/ts/rhea/rhea.config.ts
+++
@@ -1,3 +0,0 @@
-import { getConfig } from '@ref-finance/ref-sdk';// This will automatically use mainnet since no env vars are set
-const config = getConfig();
-console.log(config); // You'll see mainnet settings
