80391faa29496ba897dd9ecb2c05587c7f19f8ba
Nathan Renfroe <thesunshiningmin@gmail.com>
2025-08-31 15:31:46 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './css/main.css';
 import './ts/hello.ts';
 import './ts/near/near.config.ts';
+import './ts/rhea/rhea.config.ts';
 // app ui
 import App_section_route from './tsx/app_section_routing.tsx';
 import App_section_footer from './tsx/app_footer.tsx';
