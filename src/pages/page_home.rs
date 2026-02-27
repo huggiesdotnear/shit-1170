985ef48a3d36f1d4b2082fbcbfbc3ba2897ddd1f
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-02-27 13:15:51 -0600

    fc re

--- src/pages/page_home.rs
+++ src/pages/page_home.rs
@@ -1,28 +1,30 @@
 use dioxus::prelude::*;
+use dioxus::prelude::use_navigator;
 use gloo_timers::future::TimeoutFuture;
 use wasm_bindgen_futures::spawn_local;
-use web_sys::window;
+use crate::logic::enum_route::SHIT_APP_ROUTE;
 // =========================================
 // PAGE_HOME
 #[component]
 pub fn PAGE_HOME() -> Element {
+    let nav = use_navigator();
     use_effect(move || {
-        redirect_after(3000, "/holders");
+        redirect_to_target_after_3s(nav.clone());
     });
     rsx! {
         h1 { "HOME" }
     }
 }
 // =========================================
-// generic helper: redirect after `delay_ms` using window.location
-fn redirect_after(delay_ms: u32, path: &str) {
-    let path = path.to_string();
+// generic helper: redirect after `delay_ms` to any Route
+fn redirect_after(nav: Navigator, delay_ms: u32, target: SHIT_APP_ROUTE) {
     spawn_local(async move {
         TimeoutFuture::new(delay_ms).await;
-        if let Some(win) = window() {
-            win.location().set_href(&path).ok();
-        }
+        nav.push(target);
     });
+}
+fn redirect_to_target_after_3s(nav: Navigator) {
+    redirect_after(nav, 3000, SHIT_APP_ROUTE::PAGE_HOLDERS {});
 }
 // =========================================
 // =========================================
