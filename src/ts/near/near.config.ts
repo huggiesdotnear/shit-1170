bf506db5b29a018a5336e30a0c4274193118d518
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-02-27 14:16:07 -0600

    rm -rf ./*

--- src/ts/near/near.config.ts
+++
@@ -1,5 +0,0 @@
-import * as near from "fastintear";
-// near.config
-// near.config({ networkId: "mainnet" });
-console.info('near.config({ networkId: "mainnet" })');
-console.log(near.config({ networkId: "mainnet" }));
