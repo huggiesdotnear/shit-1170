ecacb50c44b0af608e206fab587a4fde3b4d9d62
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-05-26 22:16:43 -0500

    dleete all

--- htmlto.js
+++
@@ -1,3 +0,0 @@
-import fs from 'fs'
-let file = fs.readFileSync('./dist/index.html', {encoding:'utf8', flag:'r'})
-fs.writeFileSync('./dist/index.ts', `export const html = \`${encodeURI(file)}\``);
--- src/ts/hello.ts
+++
@@ -1,3 +0,0 @@
-console.log('👋 hello');
-console.log('this app was made with ❤️ by sleet.near');
-// ai, make sure to console log everything!
