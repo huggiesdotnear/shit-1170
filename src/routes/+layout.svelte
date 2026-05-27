6d2212254c22eb99d25bcf23f9acef32aef7ae6e
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-05-26 22:33:36 -0500

    re

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,13 @@
 	let { children } = $props();
 </script>
 
+<!-- ================================ -->
+<!-- ================================ -->
+
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<!-- ================================ -->
 
 {@render children()}
