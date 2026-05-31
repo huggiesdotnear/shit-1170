8ba8c699b045faf382ec223ef3452db2b5390ace
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-05-30 22:25:21 -0500

    rm all

--- src/ts/routes.ts
+++
@@ -1,9 +0,0 @@
-// routes.ts
-export const ROUTES = {
-    home: { path: '/', label: 'HOME' },
-    swap: { path: '/swap', label: 'SWAP' },
-    info: { path: '/shit', label: '$SHIT' },
-    huggies: { path: '/huggies', label: 'HUGGIES' },
-  } as const;
-  
-  export type RouteKey = keyof typeof ROUTES;
