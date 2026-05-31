89300a8e0a7da8291d3b238a6843207c4c905627
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-05-31 01:02:51 -0500

    Create +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
+	import { HUGGIES_SLOGANS } from "$lib/ts/slogans";
+	const getRandomSlogan = () => HUGGIES_SLOGANS[Math.floor(Math.random() * HUGGIES_SLOGANS.length)];
+	onMount(() => {
+		setTimeout(() => {
+			goto("/shit?token=shit-1170.meme-cooking.near");
+		}, 1000);
+	});
+</script>
+
+<section>
+	<h1>404</h1>
+	<p>{getRandomSlogan()}</p>
+</section>
+
+<style></style>
