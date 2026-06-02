01abc7c8880beb8f3c9efeb3577594dec896b717
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-06-01 20:52:21 -0500

    re

---
+++ src/lib/components/ft_metadata.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { ft_metadata_fun } from "@near-kit-tool-box/fun"
+  import type { FT_METADATA_TYPE } from "@near-kit-tool-box/fun"
+  // ============================================
+</script>
+
+<!-- ============================================ -->
+<!-- ============================================ -->
+
+<!-- component_blank -->
+<!-- COMPONENT_BLANK -->
+<div>
+	<h1>BLANK</h1>
+</div>
+
+<!-- ============================================ -->
+<!-- ============================================ -->
+
+<style></style>
