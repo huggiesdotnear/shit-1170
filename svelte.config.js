813410fefa181e8e10dfd80a620a4a1e00ad8aa0
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-06-04 16:37:51 -0500

    re

--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,15 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: "build",
 			assets: "build",
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		prerender: {
+			handleHttpError: "warn"
+		}
 	}
 };
 // ================================
