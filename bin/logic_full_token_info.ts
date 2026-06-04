2e9b20fc0a380cf46b57fedb434c436afba1e365
Nathan Renfroe <thesunshiningmin@gmail.com>
2026-06-03 21:57:58 -0500

    Update logic_full_token_info.ts

--- bin/logic_full_token_info.ts
+++ bin/logic_full_token_info.ts
@@ -2,12 +2,12 @@ import { full_token_info_fun } from "../src/lib/ts/full_token_info_fun";
 // ============================================
 const token_ca = "intel.tkn.near";
 // ============================================
-const price = await full_token_info_fun(token_ca);
+const full_token_info = await full_token_info_fun(token_ca);
 // ============================================
-console.log("Account ID:", price.account_id);
-console.log("Price USD:", price.price_usd);
-console.log("Metadata:", price.metadata);
-console.log("Total Supply:", price.total_supply);
-console.log("Circulating Supply:", price.circulating_supply);
-console.log("Liquidity USD:", price.liquidity_usd);
-console.log("Volume 24h:", price.volume_usd_24h);
+console.log("Account ID:", full_token_info.account_id);
+console.log("Price USD:", full_token_info.price_usd);
+console.log("Metadata:", full_token_info.metadata);
+console.log("Total Supply:", full_token_info.total_supply);
+console.log("Circulating Supply:", full_token_info.circulating_supply);
+console.log("Liquidity USD:", full_token_info.liquidity_usd);
+console.log("Volume 24h:", full_token_info.volume_usd_24h);
